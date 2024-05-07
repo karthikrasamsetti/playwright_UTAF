138cde0ccd92b2cab643edf1ef04179e64bb8ba5
karthikrasamsetti <karthikrasamsetti@gmail.com>
2024-05-07 16:02:24 +0530

    config change

--- playwright.config.js
+++ playwright.config.js
@@ -43,7 +43,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   timeout: 1000 * 60 * 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: 'html',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -59,7 +59,7 @@ module.exports = defineConfig({
       slowMo: 500,
     },
   },
-  reporter: [['@reportportal/agent-js-playwright', RPconfig],['html']],
+  // reporter: [['@reportportal/agent-js-playwright', RPconfig],['html']],
   /* Configure projects for major browsers */
   projects: [
     {
